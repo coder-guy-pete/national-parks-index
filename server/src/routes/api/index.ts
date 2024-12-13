2e436f291a9086087a5a91dca21e7d42159cd3a2
Peter Hintze <hintze.peter@gmail.com>
2024-12-13 18:41:03 -0500

    Initialize application

---
+++ server/src/routes/api/index.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+const router = Router();
+
+import parkRoutes from './parkRoutes.js';
+import historyRoutes from './historyRoutes.js';
+
+router.use('/parks', parkRoutes);
+router.use('/history', historyRoutes);
+
+export default router;
+
+// /api/parks
+// /api/history
